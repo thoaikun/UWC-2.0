a17948066d70708d5a6c4e59832cc20ff1f70d9c
Lê Nguyễn Huyền Thoại <lenguyenhuyenthoai@gmail.com>
2022-12-04 11:49:41 +0700

    change db pass

--- backend/src/app/model/model.js
+++ backend/src/app/model/model.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 const database = {
     host     : 'iu51mf0q32fkhfpl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     user     : 'gx09dow97kh2f5yb',
-    password : 'oj8bnas8uyshfedt',
+    password : 'qflhnn1x9qvb9dlu',
     database : 'ylwwsln7lqj7l71x'
 }
 
