371f9f33684a6033fc74fd0bacea2d0a1a10c172
Lê Nguyễn Huyền Thoại <lenguyenhuyenthoai@gmail.com>
2022-12-06 08:53:39 +0700

    update model password

--- backend/src/app/model/model.js
+++ backend/src/app/model/model.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 const database = {
     host     : 'iu51mf0q32fkhfpl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
     user     : 'gx09dow97kh2f5yb',
-    password : 'c4dbm733tzp4lf1k',
+    password : 'nkkf3y6te8v3bm6d',
     database : 'ylwwsln7lqj7l71x'
 }
 
